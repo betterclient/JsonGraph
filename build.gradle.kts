40a4bb415e536b8ceeec4ff524bdf87d983825b3
betterclient <iamanaltlolwhypog@gmail.com>
2024-12-18 20:20:47 +0300

    base

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import java.io.FileOutputStream
+import java.util.zip.ZipFile
+
+plugins {
+    java
+    war
+    id("org.teavm") version "0.10.2"
+}
+
+group = "io.github.betterclient"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(teavm.libs.jsoApis)
+}
+
+teavm.js {
+    addedToWebApp = true
+    mainClass = "io.github.betterclient.jsongraph.Main"
+
+    targetFileName = "jsongraph.js"
+    obfuscated = false
+}
+
+var extract = task("extract") {
+    doLast {
+        val jf = ZipFile(file("build/libs/JSONGraph-1.0.war"))
+        val entries = jf.stream().toList()
+        for (entry in entries) {
+            if (entry.name == "js/jsongraph.js") {
+                val iss = jf.getInputStream(entry)
+                val data = iss.readAllBytes()
+                iss.close()
+                val f = file("out/jsongraph.js")
+                f.delete()
+                val os = FileOutputStream(f)
+                os.write(data)
+                os.close()
+            }
+        }
+        jf.close()
+    }
+}
+
+tasks.named("build") {
+    finalizedBy(extract)
+}
